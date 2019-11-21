a1eb73649c740f2920be66594f8e7d763be1b5c3
Dompatchi <jens.scheidmann@gmail.com>
2019-11-21 15:36:24 +0100

    fdssdf

--- src/main/java/ch/noser/learningDevOps/LearningDevOpsApplication.java
+++ src/main/java/ch/noser/learningDevOps/LearningDevOpsApplication.java
@@ -2,9 +2,10 @@ package ch.noser.learningDevOps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LearningDevOpsApplication {
+public class LearningDevOpsApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningDevOpsApplication.class, args);
